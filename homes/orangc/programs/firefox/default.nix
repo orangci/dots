80c1fdb5277230972c0698bd8e19ae60b70867e6
orangc <c@orangc.net>
2025-05-20 14:52:53 +0300

    fix: iconupdateURL is deprecated

--- homes/orangc/programs/firefox/default.nix
+++ homes/orangc/programs/firefox/default.nix
@@ -29,7 +29,7 @@ in {
 
             "NixOS Wiki" = {
               urls = [{template = "https://wiki.nixos.org/index.php?search={searchTerms}";}];
-              iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+              icon = "https://wiki.nixos.org/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000; # every day
               definedAliases = ["@nw"];
             };
