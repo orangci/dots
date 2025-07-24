23da73738f0fe4f801958755f24376d1277e1991
orangc <c@orangc.net>
2025-07-24 17:37:02 +0300

    modules/server/gitea: update theme sha256 hash

--- modules/server/gitea.nix
+++ modules/server/gitea.nix
@@ -75,7 +75,7 @@ in
       let
         theme = pkgs.fetchzip {
           url = "https://github.com/catppuccin/gitea/releases/download/v1.0.2/catppuccin-gitea.tar.gz";
-          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          sha256 = "sha256-rZHLORwLUfIFcB6K9yhrzr+UwdPNQVSadsw6rg8Q7gs=";
           stripRoot = false;
         };
       in
