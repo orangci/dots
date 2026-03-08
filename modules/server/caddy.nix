7b57eb5513f65218622a89551704a8db974fa017
orangc <c@orangc.net>
2026-03-08 15:32:33 +0300

    modules/server/caddy: broken attempt at internal https

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib)
     mkEnableOption
@@ -27,6 +32,21 @@ let
       let
         base = lib.removeSuffix ".orangc.net" mod.domain;
       in
+      nameValuePair "${base}.home.orangc.net" {
+        extraConfig = mkDefault ''
+          reverse_proxy localhost:${toString mod.port}
+          tls {
+           dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+           resolvers 1.1.1.1 1.0.0.1 
+          }
+        '';
+      }
+    ) validModules)
+    (mapAttrs' (
+      _: mod:
+      let
+        base = lib.removeSuffix ".orangc.net" mod.domain;
+      in
       nameValuePair "http://${base}.home" {
         extraConfig = mkDefault ''
           reverse_proxy localhost:${toString mod.port}
@@ -40,14 +60,31 @@ in
   options.modules.server.caddy.enable = mkEnableOption "Enable Caddy";
 
   config = mkIf cfg.enable {
+    modules.common.sops.secrets.acme-dns-cloudflare-token = {
+      path = "/var/secrets/acme-dns-cloudflare-token";
+      owner = "caddy";
+    };
     services.caddy = {
       enable = true;
       email = "c@orangc.net";
+      package = pkgs.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
+        hash = "sha256-mmkziFzEMBcdnCWCRiT3UyWPNbINbpd3KUJ0NMW632w=";
+      };
+      environmentFile = config.modules.common.sops.secrets.acme-dns-cloudflare-token.path;
+      globalConfig = ''
+      acme_dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+      '';
       virtualHosts = mkMerge [
         dynamicVhosts
         {
-          "ping.home".extraConfig = "respond pong";
-          "dns.home".extraConfig = "reverse_proxy 127.0.0.1:5380";
+          "home.orangc.net".extraConfig = "
+          tls {
+           dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+           resolvers 1.1.1.1 1.0.0.1
+           }
+          ";
+          "dns.orangc.net".extraConfig = "reverse_proxy 127.0.0.1:5380";
           "mc-map.orangc.net".extraConfig =
             mkIf config.modules.server.minecraft.juniper-s10.enable "reverse_proxy localhost:${
               toString (config.modules.server.minecraft.juniper-s10.port - 2000)
