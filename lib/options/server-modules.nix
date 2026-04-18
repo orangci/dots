702cf7ae593e3e80fcd9303245210fb09b494fab
orangc <c@orangc.net>
2026-04-18 20:44:41 +0300

    lib/options/server-modules: update option descriptions

--- lib/options/server-modules.nix
+++ lib/options/server-modules.nix
@@ -10,28 +10,29 @@
 with lib;
 
 {
-  enable = mkEnableOption "Enable ${name}";
+  enable = mkEnableOption "${name}";
 
-  glance.enable = mkEnableOption "Enable visibility for this service in the Glance dashboard";
-  cloudflared.enable = mkEnableOption "Enable Cloudflare Tunnels for this service";
-  internalTailscaleDomain.enable = mkEnableOption "Enable an internal tailnet domain for this service at ${subdomain}.${flakeSettings.domains.tailnet}";
-  ntfyChecking.enable = mkEnableOption "Allow Ntfy to send notifications when this service goes down";
+  glance.enable = mkEnableOption "visibility for ${name} in the Glance dashboard";
+  cloudflared.enable = mkEnableOption "Cloudflare Tunnelling for ${name}";
+  internalTailscaleDomain.enable = mkEnableOption "an internal tailnet domain for ${name} at ${subdomain}.${flakeSettings.domains.tailnet}";
+  ntfyChecking.enable = mkEnableOption "Ntfy sending notifications when this ¢{name} goes down";
 
   name = mkOption {
     type = types.str;
     default = name;
+    description = "Set the name of the service with correct capitalisation if necessary for other modules such as modules.glance to access.";
   };
 
   port = mkOption {
     type = types.port;
     default = port;
-    description = "The port for ${name} to be hosted at";
+    description = "The port at which ${name} is hosted at";
   };
 
   subdomain = mkOption {
     type = types.str;
     default = strings.toLower subdomain;
-    description = "The domain for ${name} to be hosted at";
+    description = "The domain at which ${name} is hosted at";
   };
 
   glance.icon = mkOption {
