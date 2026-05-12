df5c458418cb582991f923f88acaa7c05d89c00e
orangc <c@orangc.net>
2026-05-12 10:24:50 +0300

    flake: dont make noctalia follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -121,10 +121,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia-shell";
   };
 
   outputs =
